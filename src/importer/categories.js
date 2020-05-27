de43d7c2cbe7c72a04c1978f09ab080fbf29d075
Aggelos <aggelos@webtrails.gr>
2020-05-27 13:39:43 +0300

    Use the command options values to fetch categories

--- src/importer/categories.js
+++ src/importer/categories.js
@@ -1,8 +1,8 @@
 const categoryTemplate = require('../templates/category')
 const sendToElastic = require('../common/sendToElastic')
 
-const importer = ({ config, elasticClient, apiConnector, logger }) => {
-  apiConnector(config).getAsync('products/categories?order_by=id').then(
+const importer = ({ config, elasticClient, apiConnector, logger, page = 1, perPage = 20 }) => {
+  apiConnector(config).getAsync(`products/categories?order_by=id&page=${page}&per_page=${perPage}`).then(
     (result) => {
       let body = result.toJSON().body
       let array = JSON.parse(body)
